6b41a88e4b2e5301a38bb6e04e2329d911246cca
Vasco21 <vascoeti123@gmail.com>
2022-07-09 05:59:48 +0200

    make it better

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use("/api/users", userRoutes);
 // --------------------------deployment------------------------------
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.resolve(process.cwd(), 'build')))
+  app.use(express.static(path.resolve(process.cwd(), 'frontend/build')))
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(process.cwd(), 'index.html'))
+      res.sendFile(path.resolve(process.cwd(), 'frontend/build/index.html'))
     }) 
   }else {
   app.get("/", (req, res) => {
