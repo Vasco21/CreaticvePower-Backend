325fb62632cc547872ddd5078704da515eaaef14
Vasco21 <vascoeti123@gmail.com>
2022-07-09 06:47:40 +0200

    make it better

--- server.js
+++ server.js
@@ -6,12 +6,23 @@ import path from "path";
 import noteRoutes from "./routes/noteRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
+import cors from "cors"
 
 dotenv.config();
 
 connectDB();
 
 const app = express(); // main thing
+
+app.use(cors());
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Headers, *, Access-Control-Allow-Origin', 'Origin, X-Requested-with, Content_Type,Accept,Authorization','http://localhost:4200');
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+});
 
 app.use(express.json()); // to accept json data
 
